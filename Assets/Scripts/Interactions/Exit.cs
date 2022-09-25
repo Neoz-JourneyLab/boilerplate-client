9e56cc7fedc2ae115858d84caa52f7471720b1d2
Neoz Sagan <discovery.game.online@gmail.com>
2022-09-25 14:53:28 +0200

    qern

--- Assets/Scripts/Interactions/Exit.cs
+++ Assets/Scripts/Interactions/Exit.cs
@@ -5,13 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour {
 	public List<string> playersHits = new List<string>();
+	bool ended = false;
 	private void OnTriggerEnter(Collider other) {
 		if (other.tag != "Player" && other.tag != "OtherPlayer") return;
 		if (other.tag == "Player" && !playersHits.Contains(uWebSocketManager.socketId)) playersHits.Add(uWebSocketManager.socketId);
 		if (other.tag == "OtherPlayer" && !playersHits.Contains(other.name)) playersHits.Add(other.name);
-		if(playersHits.Count == 2) {
-			uWebSocketManager.EmitEv("victory", new { SceneManager.GetActiveScene().name });
-			SceneManager.LoadScene("Lobby");
+		if(!ended && playersHits.Count == 2) {
+			Invoke(nameof(EndLevel), 2);
+			ended = true;
 		}
+	}
+
+	void EndLevel() {
+		uWebSocketManager.EmitEv("victory", new { SceneManager.GetActiveScene().name });
+		SceneManager.LoadScene("Lobby");
 	}
 }
--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -14,7 +14,7 @@ public class AudioManager : MonoBehaviour {
   public void Play(string alias) {
 		GameObject newSound = Instantiate(new GameObject(), transform.position + Vector3.up, Quaternion.identity);
     newSound.AddComponent<AudioSource>();
-    var clip = clips.First(c => c.name == alias);
+    var clip = clips.FirstOrDefault(c => c.name == alias);
     if(clip == null) {
       print("Audio not found : " + alias);
       return;
